72a1953d4a9fa432792f5377f533701b47699d93
Ben de Garcia <degarcia.ben@gmail.com>
2020-06-14 22:35:58 -0500

    added click listeners, and top half of right div

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,52 @@
-var APIkey = cc1cb311d206444f71b5881ecfd1d53c
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+var APIkey = "cc1cb311d206444f71b5881ecfd1d53c"
+var dateToday = moment().format('L');
+var lat = ""
+var lon = ""
+
+$(document).ready(function(){
+
+    function getFutureCast(){
+        var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIkey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            $("#uv").text("UV Index: ");
+            var uvDiv = $('<div>').text(response.current.uvi)
+            uvDiv.attr('id', 'uvDiv')
+            $("#uv").append(uvDiv)
+
+
+        })//inside AJAX
+
+    }//inside futureCast
+
+
+
+
+    $(document).on('click', '.btn', function(event){
+        event.preventDefault();
+
+        var cityName = $("input").val()
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+        let UVdiv = ('<div>')
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response)
+            $("#city-disp").text(response.name + "(" + dateToday + ")" )
+            var temp = Math.round(((response.main.temp - 273.15)*1.8 +32)*10)/10
+            $("#temp").text("Temperature: " + temp)
+            $("#humidity").text("Humidity: " + response.main.humidity + "%")
+            $("#wind-speed").text("Wind Speed: " + response.wind.speed + "MPH")
+            lon = response.coord.lon
+            lat = response.coord.lat
+            getFutureCast()
+        })//everything above this is inside the first AJAX
+
+    })//everything above this is on click of search button
+
+})
