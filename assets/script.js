d72988d9ff6e95a2a2c4f12afd33118fba1b2eb7
Ben de Garcia <degarcia.ben@gmail.com>
2020-06-17 22:52:36 -0500

    all functional sans local storage search

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var dateToday = moment().format('L');
 var lat = ""
 var lon = ""
 var weatherCards = $("#weatherCards");
+var pastSearches = []
 
 $(document).ready(function(){
 
@@ -20,30 +21,48 @@ $(document).ready(function(){
             $("#uv").append(uvDiv)
             
             for( var i = 0; i < 5; i++){
+                let add = i + 1
                 let thisAjax = response.daily[i]
+
+                let cardDiv = $("#card" + i)
                 let futureTemp = $("<div>").text("Temp: " + Math.round(((thisAjax.temp.day - 273.15)*1.8 +32)*10)/10 + "F");
+
                 let futureHumidity = $("<div>").text("Humidity: " + thisAjax.humidity + "%");
-                $(".card-body" + [i]).append(futureTemp, futureHumidity);
-                $(".futureDate" + [i]).text(moment().add([i + 1], 'days').format('L'));
-                var weatherImg = thisAjax.weather.icon
+
+                let futureDate = $("<h5>").text(moment().add(add, 'day').format('MM/DD/YYYY'));
+                
+                let weatherCode = thisAjax.weather[0].icon
+                let weatherImg = "http://openweathermap.org/img/wn/" + weatherCode + "@2x.png";
+
                 console.log(weatherImg)
-             
-
-                
-                
-                
-            }
-
+                let weatherPic = $("<img>");
+                weatherPic.attr("src", weatherImg);
+                let weatherDesc = $("<div>").text(thisAjax.weather[0].main)
+                cardDiv.append(futureDate, futureTemp, futureHumidity, weatherPic);
+            }//inside for loop
         })//inside AJAX
     }//inside futureCast
 
+    function updateLocalStorage(city){
+        pastSearches.push(city);
+        for (let i = 0; i < pastSearches.length; i++) {
+            if (pastSearches[i] === pastSearches[i-1]) pastSearches.splice(i, 1);
+        }
+        localStorage.setItem('pastSearches', pastSearches)
+    }
 
+    function pullLocalStorage(city){
+        localStorage.getItem('pastSearches')
+        pastSearches.forEach ( function (city) {
+        let searches = $('<div>');
+        searches.attr('class', 'past-searches')
+        searches.text(city);
+        $('#past-searches').append(searches);
+        });
 
+    }
 
-    $(document).on('click', '.btn', function(event){
-        event.preventDefault();
-
-        var cityName = $("input").val()
+    function getCurrentWeather(cityName){
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
         let UVdiv = ('<div>')
         $.ajax({
@@ -58,9 +77,34 @@ $(document).ready(function(){
             $("#wind-speed").text("Wind Speed: " + response.wind.speed + "MPH")
             lon = response.coord.lon
             lat = response.coord.lat
-            getFutureCast()
+            console.log(cityName)
         })//everything above this is inside the first AJAX
+        updateLocalStorage(cityName)
+        pullLocalStorage(cityName);
+        setTimeout(function(){
+            getFutureCast();
+        },
+            200);
+    }
 
+    $(document).on('click', '.btn', function(event){
+        event.preventDefault();
+        $('.card').empty()
+        var cityName = $("input").val()
+        cityName = cityName.toUpperCase()
+        getCurrentWeather(cityName)
     })//everything above this is on click of search button
 
+
+    $(document).on('click', '.past-searches', function(event){
+        event.preventDefault();
+        let pastCitySearch = $(".past-searches").text();
+        console.log(pastCitySearch)
+        getCurrentWeather(pastCitySearch)
+
+        ;
+
+    });
+
+    updateLocalStorage();
 })
